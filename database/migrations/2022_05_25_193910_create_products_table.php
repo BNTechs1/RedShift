c28f92a8853edbcafea719962ff995e1513e1e8e
jrermi <it.ermias.asmare@gmail.com>
2022-05-25 22:53:46 +0300

    created the product model, controller and migration with sample fields

--- database/migrations/2022_05_25_193910_create_products_table.php
+++ database/migrations/2022_05_25_193910_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('tag');
+            $table->string('description');
+            $table->string('price');
             $table->timestamps();
         });
     }
