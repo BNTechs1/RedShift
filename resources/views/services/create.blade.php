db3b38d09d8dae59914795298ffed6193be480f1
jrermi <it.ermias.asmare@gmail.com>
2022-06-01 10:21:11 +0300

    added the creat.blade.php view to create services

---
+++ resources/views/services/create.blade.php
@@ -0,0 +1,56 @@
+@extends('dashboard_layout')
+
+@section('content')
+
+  <main id="main" class="main">
+    <section class="section">
+      <div class="row">    
+        <div class="col-lg-6">
+           <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Add Service</h5>
+
+              <!-- Floating Labels Form -->
+              <form action="/services" method="POST"  enctype="multipart/form-data" class="row g-3">
+              @csrf
+                <div class="col-md-12">
+                  <div class="form-floating">
+                    <input type="text" name="name" value="{{old('name')}}" class="form-control" id="floatingName" placeholder="Your Name">
+                    <label for="floatingName">Service Name</label>
+                  </div>
+                  @error('name')
+                    <p>{{$message}}</p>
+                  @enderror
+                </div>
+                <div class="col-md-12">
+                  <div class="form-floating">
+                    <input class="form-control" name="image"  type="file" id="fileUpload">
+                </div>
+                    @error('image')
+                    <p>{{$message}}</p>
+                  @enderror
+                </div>
+                <div class="col-12">
+                  <div class="form-floating">
+                    <textarea class="form-control" name="description" value="{{old('description')}}"  placeholder="Description" id="floatingDescription" style="height: 100px;"></textarea>
+                    <label for="floatingDescription">Description</label>
+                  </div>
+                      @error('description')
+                    <p>{{$message}}</p>
+                  @enderror
+                </div>
+               
+                <div class="text-center">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="reset" class="btn btn-secondary">Reset</button>
+                </div>
+              </form><!-- End floating Labels Form -->
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    </main><!-- End #main -->
+
+@endsection
