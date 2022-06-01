1fb94c04a9df26b7c2692d9e43dd1e608ec3f472
jrermi <it.ermias.asmare@gmail.com>
2022-06-01 10:19:02 +0300

    loaded the services to all view pages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\Product;
+use App\Models\Service;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -25,9 +26,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $products = Product::latest()->filter(request(['tag','search']))->paginate(10);
+        $services = Service::latest()->paginate(3);
 
-        View::composer('*', function($view) use($products){
+        View::composer('*', function($view) use($products,$services){
             $view->with('products', $products);
+            $view->with('services', $services);
           });
     }
 }
