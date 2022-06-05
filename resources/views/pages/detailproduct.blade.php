d652ef279e052f9342f0f3bd89725cc0f1bdace6
jrermi <it.ermias.asmare@gmail.com>
2022-06-05 14:13:06 +0300

    ui tweek to single products page

--- resources/views/pages/detailproduct.blade.php
+++ resources/views/pages/detailproduct.blade.php
@@ -29,35 +29,42 @@
                         </div><!--/.single-about-history-->
                     </div><!--/.col-->
                     <div class="col-md-4">
-                            <div class="blog-right">
-                                <div class="blogs-widget">
-                                    <h2>categories</h2>
-                                    <div class="blog-categories-list">
-                                        <ul>
-                                            <li><a href="#">all</a></li>
-                                            <li><a href="#">busisness</a></li>
-                                            <li><a href="#">consultency</a></li>
-                                            <li><a href="#">corporate</a></li>
-                                            <li><a href="#">politics</a></li>
-                                            <li><a href="#">marketing</a></li>
-                                            <li><a href="#">lifestyle</a></li>
+                            <div class=row>
+                             <div class=" col-md-4 col-sm-6 col-xs-12">
+                                <div class="hm-footer-widget">
+                                    <div class="footer-menu ">	  
+                                        <ul class="">
+                                            <li><a href="index.html" >Millgae:  {{$product->name}}</a></li>
+                                            <li><a href="about.html">CC:{{$product->tag}}</a></li>
+                                            <li><a href="services.html">Model:{{$product->price}}</a></li>
+                                            <li><a href="portfolio.html">Engine:</a></li>
+                                            <li><a href="blog.html">wheel:</a></li>
+                                            <li><a href="contact.html">type:</a></li> 
                                         </ul>
-                                    </div><!--/.blog-categories-list-->
-                                </div><!--/.blog-widget-->
+                                    </div><!-- /.footer-menu-->
+                                </div><!--/.hm-footer-widget-->
+                            </div><!--/.col-->
+                        </div>
+                            {{-- <div class="blog-right">
+                               <div class="blogs-widget">
+										<h2>categories</h2>
+										<div class="blog-categories-list">
+											<ul>
+												@foreach ($products as $product)
+													@php 
+													$tags = explode(',',$product->tag)
+													@endphp
+													@foreach ($tags as $tag)
+														<li><a href="/products/?tag={{$tag}}">{{$tag}}</a></li>													
+													@endforeach
+												@endforeach
+											</ul>
+										</div><!--/.blog-categories-list-->
+									</div><!--/.blog-widget-->
 
-                                <div class="blogs-widget">
-                                    <h2>popular tags</h2>
-                                    <div class="blog-tags">
-                                        <button type="button" class="btn btn-default">busisness</button>
-                                        <button type="button" class="btn btn-default">consultency</button>
-                                        <button type="button" class="btn btn-default">market</button><br />
-                                        <button type="button" class="btn btn-default">sales</button>
-                                        <button type="button" class="btn btn-default">corporate</button>
-                                        <button type="button" class="btn btn-default">politics</button>
-                                    </div><!--/.blog-tags-->
-                                </div><!--/.blog-widget-->
+                                
 
-                            </div><!--/.blog-right-->
+                            </div><!--/.blog-right--> --}}
 
                     </div><!--/.col-->
                 </div><!--/.row-->
@@ -67,72 +74,7 @@
     </div><!--/.about-history-->
     <!--about-history end-->
 
-    <!--hm-footer start-->
-    <section class="hm-footer">
-        <div class="container">
-            <div class="hm-footer-details">
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class=row>
-                             <div class=" col-md-4 col-sm-6 col-xs-12">
-                                <div class="hm-footer-widget">
-                                    {{-- <div class="hm-foot-title">
-                                        <h4>Useful Links</h4>
-                                    </div><!--/.hm-foot-title--> --}}
-                                    <div class="footer-menu ">	  
-                                        <ul class="">
-                                            <li><a href="index.html" >Millgae</a></li>
-                                            <li><a href="about.html">CC</a></li>
-                                            <li><a href="services.html">Model</a></li>
-                                            <li><a href="portfolio.html">Engine</a></li>
-                                            <li><a href="blog.html">wheel</a></li>
-                                            <li><a href="contact.html">type</a></li> 
-                                        </ul>
-                                    </div><!-- /.footer-menu-->
-                                </div><!--/.hm-footer-widget-->
-                            </div><!--/.col-->
-                            <div class=" col-md-4 col-sm-6 col-xs-12">
-                                <div class="hm-footer-widget">
-                                     <div class="footer-menu ">	  
-                                        <ul class="">
-                                            <li><a href="index.html" >Millgae</a></li>
-                                            <li><a href="about.html">CC</a></li>
-                                            <li><a href="services.html">Model</a></li>
-                                            <li><a href="portfolio.html">Engine</a></li>
-                                            <li><a href="blog.html">wheel</a></li>
-                                            <li><a href="contact.html">type</a></li> 
-                                        </ul>
-                                    </div><!-- /.footer-menu-->
-                                </div><!--/.hm-footer-widget-->
-                            </div><!--/.col-->
-                        </div>
-                    </div>
-                    <div class=" col-md-4 col-sm-6  col-xs-12">
-                        <div class="hm-footer-widget">
-                            <div class="hm-foot-title">
-                                <h4> Our Newsletter</h4>
-                            </div><!--/.hm-foot-title-->
-                            <div class="hm-foot-para">
-                                <p class="para-news">
-                                    Subscribe to our newsletter to get the latest News and offers..
-                                </p>
-                            </div><!--/.hm-foot-para-->
-                            <div class="hm-foot-email">
-                                <div class="foot-email-box">
-                                    <input type="text" class="form-control" placeholder="Email Address">
-                                </div><!--/.foot-email-box-->
-                                <div class="foot-email-subscribe">
-                                    <button type="button" >go</button>
-                                </div><!--/.foot-email-icon-->
-                            </div><!--/.hm-foot-email-->
-                        </div><!--/.hm-footer-widget-->
-                    </div><!--/.col-->
-                </div><!--/.row-->
-            </div><!--/.hm-footer-details-->
-        </div><!--/.container-->
-
-    </section><!--/.hm-footer-details-->
-    <!--hm-footer end-->
+   
 
 
  @endsection
